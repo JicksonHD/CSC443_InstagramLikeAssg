a67a6f968fbd83474a964de8ae5b0ec1093f074b
JicksonHD <jacques.ghajar@lau.edu>
2022-11-15 21:22:02 +0200

    Making sure that all attributes used have values

--- Backend/update_profile.php
+++ Backend/update_profile.php
@@ -4,6 +4,13 @@ include("db_connection.php");
 
 $response = [];
 
+if (isset($_POST['id']) && isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email'])){
 
+}
+else{
+    $response ["Error"] = "Some field are required";
+    echo json_encode($response);
+    exit();
+}
 
 ?>
