f4da8a1fd923e0401dbc6f0c2fa7a11733c56a4d
JicksonHD <jacques.ghajar@lau.edu>
2022-11-15 20:13:29 +0200

    Added handling for if user exists or not

--- Backend/get_profile.php
+++ Backend/get_profile.php
@@ -2,5 +2,17 @@
 
 include("db_connection.php");
 
+$response = [];
+
+if (isset($_GET['user_id'])) {
+
+    
+}
+else{
+    $response ["Error"] = "User not found ! ";
+    echo json_encode($response);
+    exit();
+}
+?>
 
 ?>
