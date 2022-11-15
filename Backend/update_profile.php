b4b5173502f1c34355115755b5c4641e81feec7a
JicksonHD <jacques.ghajar@lau.edu>
2022-11-15 22:13:57 +0200

    Finished update_profile api

--- Backend/update_profile.php
+++ Backend/update_profile.php
@@ -16,6 +16,41 @@ if (isset($_POST['user_id']) && isset($_POST['first_name']) && isset($_POST['las
         echo json_encode($response);
         exit();
     }
+
+    else{
+        // Check if user with this id exist in database
+        $query1 = $mysqli->prepare("SELECT * FROM users WHERE user_id = ?");
+        $query1->bind_param("i", $user_id);
+        $query1->execute();
+        $result1 = $query1->get_result();
+
+        if (mysqli_num_rows($result1) == 0) {
+            $response ["Error"] = "Invalid User";
+            echo json_encode($response);
+            exit();
+        }
+        else{
+            // Check if user with the new email exist
+            $query2 = $mysqli->prepare("SELECT * FROM users WHERE email = ? AND user_id != ?");
+            $query2->bind_param("si", $email, $user_id);
+            $query2->execute();
+            $result2 = $query2->get_result();
+            if (mysqli_num_rows($result2) != 0) {
+                $response ["Error"] = "This email is already registered";
+                echo json_encode($response);
+                exit();
+            }
+            else{
+                //Insert new email by updating db
+                $query = $mysqli->prepare("UPDATE users SET first_name = ?, last_name = ?, email = ? WHERE user_id = ?");
+                $query->bind_param("ssss", $first_name, $last_name, $email, $user_id);
+                $query->execute();
+                $response ["Success"] = "Profile Updated";
+                echo json_encode($response);
+                exit();
+            }
+        }
+    }
 }
 else{
     $response ["Error"] = "Some field are required";
