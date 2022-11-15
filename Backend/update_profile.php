d3fdebe371ec1c24615a153234dd7941c7e6f8bf
JicksonHD <jacques.ghajar@lau.edu>
2022-11-15 21:31:43 +0200

    Added condition to make sure that user is giving all the inputs

--- Backend/update_profile.php
+++ Backend/update_profile.php
@@ -4,8 +4,18 @@ include("db_connection.php");
 
 $response = [];
 
-if (isset($_POST['id']) && isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email'])){
+if (isset($_POST['user_id']) && isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email'])){
 
+    $user_id = $_POST['user_id'];
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+
+    if (empty($user_id) || empty($first_name) || empty($last_name) || empty($email)) {
+        $response ["Error"] = "Some fields are empty";
+        echo json_encode($response);
+        exit();
+    }
 }
 else{
     $response ["Error"] = "Some field are required";
