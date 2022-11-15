a10349170ab1a17fefc7b6bfc76e4bb03a069015
JicksonHD <jacques.ghajar@lau.edu>
2022-11-15 17:51:26 +0200

    Initial Commit

---
+++ Backend/db_connection.php
@@ -0,0 +1,14 @@
+
+<?php
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = null;
+$db_name = "instagramlike_db";
+
+// Establishing connection between the database and the apis
+$mysqli = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if(!$mysqli){
+    die("Connection_failed!");
+}
+?>
