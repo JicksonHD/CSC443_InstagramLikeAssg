9048a17c027d86cd3a83817a4fbd61a350bbf5f5
JicksonHD <jacques.ghajar@lau.edu>
2022-11-16 21:53:39 +0200

    Finished deleteComments api and included it in delete image api

--- Backend/deleteComments.php
+++ Backend/deleteComments.php
@@ -7,7 +7,38 @@ $response = [];
 
 if(isset($_GET['comments_id'])){
 
-    
+    $comments_id = $_GET['comments_id'];
+
+    if (empty($comments_id)) {
+        $response ["Error"] = "Id is empty";
+        echo json_encode($response);
+        exit();
+
+    }else{
+
+        // Check if comment id exists
+     $query = $mysqli->prepare("SELECT * FROM comments WHERE comments_id = ?");
+     $query->bind_param("i",$comments_id);
+     $query->execute();
+     $result = $query->get_result();
+
+     if(mysqli_num_rows($result) != 0){   
+
+        $query2 = $mysqli->prepare("DELETE FROM comments WHERE comments_id = ?");
+        $query2->bind_param("i",$comments_id);
+        $query2->execute();
+        $response["Success"] = " Your comment is deleted !";
+        echo json_encode($response);
+        exit();
+
+        }else{
+
+            $response["Error"] = "Comment does not exist from the first place ";
+            echo json_encode($response);
+            exit();
+        }
+
+    }
 
 }else{
     $response ["Error"] = "id has empty value!";
--- Backend/deleteImage.php
+++ Backend/deleteImage.php
@@ -3,13 +3,14 @@
 
 
 include("db_connection.php");
+include("deleteComments.php");
 
 
 $response = [];
 
 
 if(isset($_GET['image_id'])){
-    echo("Hellooo");
+    
     //Checking if id has a value in database or not
     $image_id = $_GET['image_id'];
     if (empty($image_id)) {
@@ -19,7 +20,7 @@ if(isset($_GET['image_id'])){
     }
     else{
 
-     // Check if user with this id exist
+     // Check if image id with this id exist
      $query = $mysqli->prepare("SELECT * FROM images WHERE image_id = ?");
      $query->bind_param("i",$image_id);
      $query->execute();
@@ -38,9 +39,33 @@ if(isset($_GET['image_id'])){
         $query2->execute();
         $response["Success"] = " Your image is deleted !";
         echo json_encode($response);
+
+         // Check if comment id exists
+        $query = $mysqli->prepare("SELECT * FROM comments WHERE comments_id = ?");
+        $query->bind_param("i",$comments_id);
+        $query->execute();
+        $result = $query->get_result();
+
+        if(mysqli_num_rows($result) != 0){   
+
+        $query2 = $mysqli->prepare("DELETE FROM comments WHERE comments_id = ?");
+        $query2->bind_param("i",$comments_id);
+        $query2->execute();
+        $response["Success"] = " Your comment is deleted !";
+        echo json_encode($response);
         exit();
+
+        }else{
+
+        $response["Error"] = "Comment does not exist from the first place ";
+        echo json_encode($response);
+        exit();
+
         }
-        else{
+
+        //Check if like id exists
+
+    }else{
         $response["Error"] = "Image not found";
         echo json_encode($response);
         exit();
