17d1ed21078fe8c04642c148f9c17f3456cb4bb4
JicksonHD <jacques.ghajar@lau.edu>
2022-11-16 17:59:37 +0200

    Added condition to make sure that the attributes in the addImage api has a value

--- Backend/addImage.php
+++ Backend/addImage.php
@@ -2,4 +2,27 @@
 
 include("db_connection.php");
 
+
+$response = [];
+
+if (isset($_POST['user_id']) && isset($_FILES['url']) && isset($_POST['description']) && isset($_POST['submit'])){
+
+    $user_id = $_POST['user_id'];
+    $description = $_POST['description'];
+
+    $img_name = $_FILES['url']['name'];
+    $img_size = $_FILES['url']['size'];
+    $tmp_name = $_FILES['url']['tmp_name'];
+    $error = $_FILES['url']['error'];
+
+    
+
+}
+else{
+    $response ["Error"] = "Some field are required";
+    echo json_encode($response);
+    exit();
+}
+
+
 ?>
