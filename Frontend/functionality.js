3b65680fe532a168646d41d8df9be1c61d8acc94
JicksonHD <jacques.ghajar@lau.edu>
2022-11-21 21:32:24 +0200

    Added function for x icon

--- Frontend/functionality.js
+++ Frontend/functionality.js
@@ -1,6 +1,8 @@
 
 const like_images = document.querySelectorAll(".heart_img");
 const comment_icons = document.querySelectorAll(".comment");
+const add_comment = document.getElementById("add_comment");
+const remove_comment_box = document.getElementById("x_icon");
 
 const imageHandler = (e) => {
     if (e.srcElement.attributes[2].value == "./empty_heart.png"){
@@ -10,6 +12,20 @@ const imageHandler = (e) => {
         e.srcElement.attributes[2].value = "./empty_heart.png";
     }
 };
-like_images.forEach(b => b.addEventListener("click",imageHandler));
 
-console.log(like_images);
+
+const commentHandler = (c) => {
+
+    add_comment.style.display = "flex";
+
+};
+
+const xicon = (x) => {
+
+    add_comment.style.display = "none";
+}
+
+like_images.forEach(b => b.addEventListener("click",imageHandler));
+comment_icons.forEach(b => b.addEventListener("click",commentHandler));
+remove_comment_box.addEventListener("click", xicon);
+
