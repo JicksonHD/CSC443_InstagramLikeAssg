550405e45f9b7f24e8e1e6a184a4b00935c4e77c
JicksonHD <jacques.ghajar@lau.edu>
2022-11-21 20:45:35 +0200

    Added heart animation using js

--- Frontend/functionality.js
+++ Frontend/functionality.js
@@ -1,7 +1,13 @@
 
+const like_images = document.querySelectorAll(".heart_img");
+const imageHandler = (e) => {
+    if (e.srcElement.attributes[2].value == "./empty_heart.png"){
+        e.srcElement.attributes[2].value = "./full_heart.png";
+    }
+    else{
+        e.srcElement.attributes[2].value = "./empty_heart.png";
+    }
+};
+like_images.forEach(b => b.addEventListener("click",imageHandler));
 
-const like_icons = document.querySelectorAll("full-heart-placement");
-
-const like (e) => {
-
-}
+console.log(like_images);
